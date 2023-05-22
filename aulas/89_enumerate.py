2b7e740f437760c3296d6a6cfc79d6c5cfe1f18b
Alison Lovegood <alisondlovegood@gmail.com>
2023-05-22 18:58:35 -0300

    aula sobre enumerate

---
+++ aulas/89_enumerate.py
@@ -0,0 +1,14 @@
+lista = ["Lucy", "Natsu", "Happy"]
+
+# iterator
+# lista_enumerada = list(enumerate(lista))
+# print(lista_enumerada)
+
+# for i, j in enumerate(lista):
+#     print(i, j)
+
+# para entender melhor
+# for tupla_enumerada in enumerate(lista):
+#     print ("FOR DA TUPLA: ")
+#     for valor in tupla_enumerada:
+#         print(f'\t{valor}')
